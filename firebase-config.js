0a3ddee507fd48e8ca8150974b2dd2e2a1ca8495
similockdn <jackphamvn@gmail.com>
2026-06-10 10:20:30 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
-
-export const firebaseConfig = {
-apiKey: "AIzaSyAn0KxCUWbD-PieOL6Ro26rEXd_B9F1GnM",
+const firebaseConfig = {
+  apiKey: "AIzaSyAn0KxCUWbD-PieOL6Ro26rEXd_B9F1GnM",
   authDomain: "smilockdng.firebaseapp.com",
   projectId: "smilockdng",
   storageBucket: "smilockdng.firebasestorage.app",
   messagingSenderId: "245518310247",
   appId: "1:245518310247:web:9568fd36149523aa626dc2"
 };
+
+export default firebaseConfig;
